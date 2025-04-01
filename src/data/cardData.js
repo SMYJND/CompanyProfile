5918d66da94a781134ebf4c001e8cd42d322e90a
SMYJND <junaidsumaiya1999@gmail.com>
2025-04-02 00:32:55 +0530

    1st phase

---
+++ src/data/cardData.js
@@ -0,0 +1,40 @@
+export const cardDataArray = [
+  {
+    cardNo: "01",
+    cardTitle: "Development",
+    listItems: ["UIUX", "DevOps", "Web design", "Frontend dev", "Backend dev"],
+  },
+  {
+    cardNo: "02",
+    cardTitle: "MSP",
+    listItems: [
+      "Infrastructure",
+      "Managed monitoring",
+      "Business Continuity",
+      "Managed Cyber",
+    ],
+  },
+  {
+    cardNo: "03",
+    cardTitle: "Cloud",
+    listItems: [
+      "AWS",
+      "Azure",
+      "Multi-Cloud",
+      "SRE Strategy",
+      "Serverless",
+      "Hybrid Strategy",
+      "Containerisation",
+    ],
+  },
+  {
+    cardNo: "04",
+    cardTitle: "Cyber",
+    listItems: [
+      "Cyber Audit",
+      "Cyber governance",
+      "Security as a service",
+      "Penetration Testing",
+    ],
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{astro,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1600px",
+        xl: "1440px",
+        lg: "1280px",
+        md: "1024px",
+        smLg: "992px",
+        tablet: "834px",
+        smMd: "768px",
+        xs: "430px",
+      },
+    },
   },
   plugins: [],
 };
